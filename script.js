3244ec0921c13dd85a6796605659932a536715b0
Thatha Hemavenkat Nagateja <nagateja.21mic7183@vitapstudent.ac.in>
2023-01-15 17:28:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+let stopLight = document.getElementById("stopLight");
+let readyLight = document.getElementById("readyLight");
+let goLight = document.getElementById("goLight");
+let stopButton = document.getElementById("stopButton");
+let readyButton = document.getElementById("readyButton");
+let goButton = document.getElementById("goButton");
+
+function stop() {
+    stopLight.style.backgroundColor = "#cf1124"
+    readyLight.style.backgroundColor = "#4b5069"
+    goLight.style.backgroundColor = "#4b5069"
+    stopButton.style.backgroundColor = "#cf1124"
+    readyButton.style.backgroundColor = "#1f1d41"
+    goButton.style.backgroundColor = "#1f1d41"
+}
+
+function ready() {
+    stopLight.style.backgroundColor = "#4b5069"
+    readyLight.style.backgroundColor = "#f7c948"
+    goLight.style.backgroundColor = "#4b5069"
+    stopButton.style.backgroundColor = "#1f1d41"
+    readyButton.style.backgroundColor = "#f7c948"
+    goButton.style.backgroundColor = "#1f1d41"
+}
+
+function go() {
+    stopLight.style.backgroundColor = "#4b5069"
+    readyLight.style.backgroundColor = "#4b5069"
+    goLight.style.backgroundColor = "#199473"
+    stopButton.style.backgroundColor = "#1f1d41"
+    readyButton.style.backgroundColor = "#1f1d41"
+    goButton.style.backgroundColor = "#199473"
+}
